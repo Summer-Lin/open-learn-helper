136bd26bdf07112d8dfa32de9cf8577e12091b63
shuai w <x@ousui.org>
2019-05-28 21:43:45 +0800

    Update open-learn-helper.user.js

--- open-learn-helper.user.js
+++ open-learn-helper.user.js
@@ -19,11 +19,74 @@
 
         // 这里执行比较快，需要延迟执行
         setTimeout(crack_common, 3000);
+        
+        add_show_btn();
     }
 
     function init(common) {
         alert(1);
         alert(common)
+    }
+    
+    function add_show_btn() {
+        $('.score').append('<span class="marginr30" style="color: blue;" pp>SHOW GO</span>');
+        $('.score span[pp]')
+            .css({
+            "cursor": "pointer",
+            "color": "blue"
+               })
+            .on('click', do_tags);
+    }
+    
+     function do_tags() {
+         $('.qestitle').each(function(i, e){
+             
+             var cont = $(e).parents('.question-cont');
+             if (!!cont.attr('_init')) {
+                 return;
+             }
+             cont.attr('_init', true);
+             
+             var question = $(e).text();
+             var equestion = encodeURIComponent(question);
+
+             var outer = cont.find('.qes-title');
+             outer.append('<a ousui-query>').find('a[ousui-query]').css({
+                 "font-weight": 900,
+                 "padding": "0px 5px",
+                 "color": "#efefef",
+                 "cursor": "pointer",
+                 "background": "#ff0000",
+                 "text-decoration": "none"
+             }).attr('href', function(){
+                 return "http://www.baidu.com/s?wd="+ equestion;
+             }).attr('target', "_blank").html('BAIDU!');
+
+
+         });
+    }
+
+    function request_monitoring() {
+        $(document).ajaxComplete(
+            function(event, xhr, settings) {
+
+                if (settings.url.indexOf('OnlineJob/DoHomework') <= 0) {
+                    return;
+                }
+                $('.qestitle').each(function(i, e){
+                    var question = $(e).text();
+                    var equestion = encodeURIComponent(question);
+                    var td = $(e).attr('style', '');
+                    td.parent().prepend('<td><a href="http://www.baidu.com/s?wd='+ question +'" target="_blank">Q</a></td>');
+                    var height = td.height();
+
+                    td.html('<textarea readonly style="width: 100%; border:0; font-weight:400;">'+question+'</textarea>').find('textarea').height(height);
+                    // 太丑
+                    // var squestion = question.replace(' ）', ' ').replace('（', '').replace('。', '');
+                    // td.parent().parent().append('<tr><td colspan="3"><input style="width: 100%; height:20px; font-size: 14px;font-weight:100;" value="' + squestion + '" /></td></tr>');
+                });
+            }
+        );
     }
 
     // 破解常规限制：右键、 ctrl+c、 选中禁用
